2ec7c28a130bfe329f9d4eb6468772f5d7bd58f9
bcwan <bcwan8@gmail.com>
2021-03-27 18:32:31 -0700

    Dry and modularize crush function

--- src/game.js
+++ src/game.js
@@ -29,41 +29,51 @@ class Game {
 
   crushOrbs(board) {
     // board cannot be crushed and is in equilibrium
-    let done = false;
-    
+    let done = false; 
     // crush the rows
-    for (let row = 0; row < board.length; row++) {
-        for (let col = 0; col < board[row].length - 2; col++) {
-            // three in a row window
-            let num1 = Math.abs(board[row][col]);
-            let num2 = Math.abs(board[row][col + 1]);
-            let num3 = Math.abs(board[row][col + 2]);
-            if (num1 === num2 && num2 === num3 && num1 !== 0) {
-                board[row][col] = -num1;
-                board[row][col + 1] = -num2;
-                board[row][col + 2] = -num3;
-                done = true;
-            }
-        }
-    }
-    
+    crushRows(board, done);
     // crush the columns
-    for (let col = 0; col < board[0].length; col++) {
-        for (let row = 0; row < board.length - 2; row++) {
-            // three in a column
-            let num1 = Math.abs(board[row][col]);
-            let num2 = Math.abs(board[row + 1][col]);
-            let num3 = Math.abs(board[row + 2][col]);
-            if (num1 === num2 && num2 === num3 && num1 !== 0) {
-                board[row][col] = -num1;
-                board[row + 1][col] = -num2;
-                board[row + 2][col] = -num3;
-                done = true;
-            }
-        }
-    }
-    
+    crushColumns(board, done);
     // gravity
+    gravity(board)
+    return done ? crushOrbs(board) : board;
+  }
+
+  crushRows(board, done) {
+    for (let row = 0; row < board.length; row++) {
+      for (let col = 0; col < board[row].length - 2; col++) {
+          // three in a row window
+          let num1 = Math.abs(board[row][col]);
+          let num2 = Math.abs(board[row][col + 1]);
+          let num3 = Math.abs(board[row][col + 2]);
+          if (num1 === num2 && num2 === num3 && num1 !== 0) {
+              board[row][col] = -num1;
+              board[row][col + 1] = -num2;
+              board[row][col + 2] = -num3;
+              done = true;
+          }
+      }
+    }
+  }
+
+  crushColumns(board, done) {
+    for (let col = 0; col < board[0].length; col++) {
+      for (let row = 0; row < board.length - 2; row++) {
+          // three in a column
+          let num1 = Math.abs(board[row][col]);
+          let num2 = Math.abs(board[row + 1][col]);
+          let num3 = Math.abs(board[row + 2][col]);
+          if (num1 === num2 && num2 === num3 && num1 !== 0) {
+              board[row][col] = -num1;
+              board[row + 1][col] = -num2;
+              board[row + 2][col] = -num3;
+              done = true;
+          }
+      }
+    }
+  }
+
+  gravity(board) {
     for (let col = 0; col < board[0].length; col++) {
         let idx = board.length - 1;
         for (let row = board.length - 1; row >= 0; row--) {
@@ -79,8 +89,6 @@ class Game {
             idx--;
         }
     }
-    
-    return done ? crushOrbs(board) : board;
   }
 }
 
